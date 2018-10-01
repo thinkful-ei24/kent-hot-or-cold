270c64c52ad1dea4abde80e2940e64ab29534852
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-01 12:39:06 -0400

    Add navigation text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         {/* Navigation
           InfoBox?
           Logo
---
+++ src/components/navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <nav className="navigation">
+        <ul>
+          <li>What?</li>
+          <li>+ New Game</li>
+        </ul>
+      </nav>
+    );
+  }
+}
